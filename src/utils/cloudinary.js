e51fd44d76dd3fa9648534328401e70faffba5dc
Kundan-programmer <csekundan2026@gmail.com>
2025-04-28 05:34:31 +0530

    cloudanry and multer middle wares

---
+++ src/utils/cloudinary.js
@@ -0,0 +1,28 @@
+import {v2 as cloudinary} from cloudinary
+import fs from "fs" // fs mean file system
+
+cloudinary.config({
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+  api_key: process.env.CLOUDINARY_CLOUD_KEY,
+  api_secret: process.env.CLOUDINARY_CLOUD_SECRET
+});
+
+const uploadOnCloudinary = async (localFilePath) =>{
+  try {
+    if (!localFilePath) return null;
+    //upload the file on cloudnary
+     const response = await cloudinary.uploader.upload(localFilePath,{
+      resoure_type:"auto"
+    })
+
+    // file has been sucessfully uploaded
+    console.log("Files is uploaded sucessfully",response.url);
+    return response;
+    
+  } catch (error) {
+    fs.unlinkSync(localFilePath) // remove the local saved filed as the uploaded operation
+    return null;
+  }
+}
+
+export {uploadOnCloudinary}
